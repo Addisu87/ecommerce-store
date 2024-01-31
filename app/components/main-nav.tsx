a4f02001d48ba8c800f0cfa6443bc5bad5a166a7
Addisu <addisuhaile87@gmail.com>
2024-01-31 11:49:53 +0300

    Update main nav alignment

--- app/components/main-nav.tsx
+++ app/components/main-nav.tsx
@@ -9,6 +9,7 @@ import { Category } from "@/types";
 interface MainNavProps {
   data: Category[];
 }
+
 const MainNav: React.FC<MainNavProps> = ({ data }) => {
   const pathname = usePathname();
 
@@ -19,7 +20,7 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
   }));
 
   return (
-    <nav className="mx-6 items-center space-x-4 lg:space-x-6 ">
+    <nav className="mx-6 flex items-center space-x-4 lg:space-x-6 ">
       {routes.map((route) => (
         <Link
           key={route.href}
